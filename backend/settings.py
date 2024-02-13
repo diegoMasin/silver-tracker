70c4ba60686252751b1b61f147712cb903f8af6e
Diego Masin <diegoifce@gmail.com>
2024-02-12 21:38:20 -0300

    inserindo configurações para funcionamento em produção.

--- backend/settings.py
+++ backend/settings.py
@@ -50,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -126,11 +129,11 @@ DECIMAL_SEPARATOR = ','
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media')
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
